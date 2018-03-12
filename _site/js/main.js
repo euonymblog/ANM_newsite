28d6f6bff7ee79c712e9792bcd1e560c35b73429
euonymblog <antoniamochan@gmail.com>
2018-03-12 21:14:53 +1100

    added menu and anchor tags

---
+++ _site/js/main.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function () {
+	//Put your code here
+  var pictures = [
+    'images/gallery/bridge.JPG',
+    'images/gallery/class.jpg',
+    'images/gallery/nepal-landscape.jpg',
+    'images/gallery/plaque.JPG',
+    'images/gallery/school-crowd.JPG',
+    'images/gallery/shelter.jpg',
+    'images/gallery/prayerflags.jpg',
+    'images/gallery/kidsontree.jpg',
+    'images/gallery/b&w_kids.jpg'
+
+  ];
+
+
+  function randomNumber(min, max) {
+    return (Math.floor(Math.random() * max) + min);
+  }
+
+  function RandomPic () {
+    return "url(" + pictures[randomNumber(1, pictures.length) - 1] + ")";
+  }
+
+    for(var i=0; i <= 4; i++) {
+      $(".gallery"+i).css("background-image", RandomPic())
+    }
+
+});
